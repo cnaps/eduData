0ee8ad074905397994df50e7ecc13eedb33170c3
haesiku <haesiku@gmail.com>
2022-04-26 10:11:42 +0900

    Create SwaggerConfig.java

---
+++ SwaggerConfig.java
@@ -0,0 +1,12 @@
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+          .select()
+          .apis(RequestHandlerSelectors.any())
+          .paths(PathSelectors.any())
+          .build();
+    }
+}
